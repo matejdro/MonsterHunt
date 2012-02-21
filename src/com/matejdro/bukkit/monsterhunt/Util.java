7f0861e2c80d8822bd66c2206bb5df3a7d0adaea
matejdro <Uporabnik@KOMP>
2012-02-21 21:01:04 +0100

    Updated message coloring/linebreak function

--- src/com/matejdro/bukkit/monsterhunt/Util.java
+++ src/com/matejdro/bukkit/monsterhunt/Util.java
@@ -24,31 +24,35 @@ public class Util {
 	
 	public static void Message(String message, Player player)
 	{
-		String color = "\u00A7f";
-		message = message.replaceAll("(&([a-z0-9]))", "§$2").replace("\\\\\u00A7", "&");
+		message = message.replaceAll("\\&([0-9abcdef])", "§$1");
+		
+		String color = "f";
 		final int maxLength = 61; //Max length of chat text message
         final String newLine = "[NEWLINE]";
         ArrayList<String> chat = new ArrayList<String>();
-        chat.add(0, color);
+        chat.add(0, "");
         String[] words = message.split(" ");
         int lineNumber = 0;
         for (int i = 0; i < words.length; i++) {
                 if (chat.get(lineNumber).length() + words[i].length() < maxLength && !words[i].equals(newLine)) {
-                        chat.set(lineNumber, chat.get(lineNumber) + " " + words[i]);
+                        chat.set(lineNumber, chat.get(lineNumber) + (chat.get(lineNumber).length() > 0 ? " " : "§" + color ) + words[i]);
+
+                        if (words[i].contains("§")) color = Character.toString(words[i].charAt(words[i].indexOf("§") + 1));
                 }
                 else {
                         lineNumber++;
                         if (!words[i].equals(newLine)) {
-                                chat.add(lineNumber, color + words[i]);
+                                chat.add(lineNumber,  "§" + color + words[i]);
                         }
                         else
-                                chat.add(lineNumber,color);
+                                chat.add(lineNumber, "");
                 }
         }
         for (int i = 0; i < chat.size(); i++) {
                 player.sendMessage(chat.get(i));
         }
 	}
+
 	public static void Broadcast(String message)
 	{
 		for (Player i : MonsterHunt.instance.getServer().getOnlinePlayers())
